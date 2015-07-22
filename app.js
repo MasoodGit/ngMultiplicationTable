a3e95c47fd68c33466ff5fbe94c6dc74f83590ef
MasoodGit <mohamed.alam78@gmail.com>
2015-07-22 07:12:00 +0530

    add numberLimit , function to compute numbers

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
 angular.module('myApp',[])
-.controller('multableCtrl',function($scope){
-  $scope.numbers = [1,2,3,4,5,6,7,8,9,10];
+.controller('multableCtrl',function($scope, $attrs) {
+  
+$scope.numberLimit = $attrs.intialNumberLimit || 10;
+
+  $scope.numbers = function() {
+    var numbers =[];
+    for(var i=0; i<$scope.numberLimit;i++) {
+      numbers[i] = i + 1;
+    };
+    return numbers;
+  };
+
+  $scope.compute = function(a,b) {
+    return a * b;
+  };
+
 });
